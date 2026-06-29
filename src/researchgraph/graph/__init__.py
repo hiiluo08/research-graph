a2eedbd713d1d3edbf7e9d1a260638c89ade8a0d
hopeSo00810 <luong.nguyenhuy@hcmut.edu.vn>
2026-06-29 14:32:47 +0700

    update init

--- src/researchgraph/graph/__init__.py
+++ src/researchgraph/graph/__init__.py
@@ -1,10 +1,9 @@
 """ LangGraph state, reducer, router and builder """
+__all__ = [
+    'append_unique_by_id',
+    'keep_latest',
+    'merge_dict',
+    'merge_sources_by_id',
+]
 
-# from .reducers import append_unique_by_id, keep_latest, merge_dict, merge_sources_by_id
-
-# __all__ = [
-#     append_unique_by_id,
-#     keep_latest,
-#     merge_dict,
-#     merge_sources_by_id,
-# ]
+from .reducers import append_unique_by_id, keep_latest, merge_dict, merge_sources_by_id
